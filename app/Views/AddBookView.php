cb8ff2e47ea651571e9710b10df2fedede3e8b8b
Angelique Faye <faye.angelique.d@gmail.com>
2020-04-07 20:31:06 +0200

    add book view and controller separation

--- app/Views/AddBookView.php
+++ app/Views/AddBookView.php
@@ -1,3 +1,89 @@
 <?php
 
 include(__DIR__ . "/../Controllers/AddBookController.php");
+
+$logged_in_add_book_view = '<main>
+      <div class="container">
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlFile1">Couverture</label>
+            <input
+              type="file"
+              class="form-control-file"
+              id="exampleFormControlFile1"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">ISBN</label>
+            <input
+              type="number"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+              >We\'ll never share your email with anyone else.</small
+            >
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Titre</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+              >We\'ll never share your email with anyone else.</small
+            >
+          </div>
+
+          <div class="form-group">
+            <label for="exampleInputEmail1">Auteur</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+              >We\'ll never share your email with anyone else.</small
+            >
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Genre</label>
+            <input
+              type="text"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <small id="emailHelp" class="form-text text-muted"
+              >We\'ll never share your email with anyone else.</small
+            >
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Description</label>
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+          <input
+            class="btn btn-primary btn-lg btn-block"
+            type="submit"
+            value="Ajouter le livre"
+          />
+        </form>
+      </div>
+    </main>
+    ';
+
+$logged_out_add_book_view = "<main>
+    <div class=\"container\">
+    <p>Vous devez être connecté(e) pour ajouter un livre.</p><a href=" . "http://$_SERVER[HTTP_HOST]" . "/se-connecter/" . ">Connectez-vous ici : connexion</a><p>Vous êtes un nouvel utilisateur ? <a href=" . "http://$_SERVER[HTTP_HOST]" . "/inscription/" . ">Inscrivez-vous ici: inscription.</a></p>
+    </div>
+    </main>";
+
+display_add_book_view($logged_in_add_book_view, $logged_out_add_book_view);
--- app/Views/AllBooksView.php
+++ app/Views/AllBooksView.php
@@ -1,52 +1,58 @@
 <main>
     <div class="container">
         <section class="books-list">
-            <article>
-                <header>
-                    <img src="" alt="">
-                    <h4>Title</h4>
-                    <p>Author, Genre</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
-                    <footer>
-                        <p>Ajouté par : User</p>
-                    </footer>
-                </header>
-            </article>
+            <h2>Voir tous les livres</h2>
+            <div class="d-flex">
+                <article>
+                    <header>
+                        <img src="" alt="">
+                        <h4>Title</h4>
+                        <p>Author, Genre</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
+                        <footer>
+                            <p>Ajouté par : User</p>
+                        </footer>
+                    </header>
+                </article>
 
-            <article>
-                <header>
-                    <img src="" alt="">
-                    <h4>Title</h4>
-                    <p>Author, Genre</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
-                    <footer>
-                        <p>Ajouté par : User</p>
-                    </footer>
-                </header>
-            </article>
-            <article>
-                <header>
-                    <img src="" alt="">
-                    <h4>Title</h4>
-                    <p>Author, Genre</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
-                    <footer>
-                        <p>Ajouté par : User</p>
-                    </footer>
-                </header>
-            </article>
-            <article>
-                <header>
-                    <img src="" alt="">
-                    <h4>Title</h4>
-                    <p>Author, Genre</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
-                    <footer>
-                        <p>Ajouté par : User</p>
-                    </footer>
-                </header>
-            </article>
+                <article>
+                    <header>
+                        <img src="" alt="">
+                        <h4>Title</h4>
+                        <p>Author, Genre</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
+                        <footer>
+                            <p>Ajouté par : User</p>
+                        </footer>
+                    </header>
+                </article>
+                <article>
+                    <header>
+                        <img src="" alt="">
+                        <h4>Title</h4>
+                        <p>Author, Genre</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
+                        <footer>
+                            <p>Ajouté par : User</p>
+                        </footer>
+                    </header>
+                </article>
+                <article>
+                    <header>
+                        <img src="" alt="">
+                        <h4>Title</h4>
+                        <p>Author, Genre</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error possimus velit, ipsa optio beatae hic quos culpa ea quod ab placeat repudiandae voluptatibus magni nisi sunt quae labore facere molestiae!</p>
+                        <footer>
+                            <p>Ajouté par : User</p>
+                        </footer>
+                    </header>
+                </article>
+            </div>
 
         </section>
+
+        <button type="button" class="btn btn-outline-primary btn-lg btn-block">Voir plus</button>
+
     </div>
 </main>
