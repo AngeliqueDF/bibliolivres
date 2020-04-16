954042b72920db568a02e5a30f9f5b6631a607ad
Angelique Faye <faye.angelique.d@gmail.com>
2020-04-16 12:59:39 +0200

    reload on changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
 let gulp = require("gulp");
+const browserSync = require('browser-sync').create();
+var terser = require('terser');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var cssnano = require('cssnano');
 
-//php
 
 //css
+//cssnano
 
 //js
+//terser
 
+function watch() {
+    browserSync.init({
+        proxy: "http://localhost:8888/"
+    });
+    gulp.watch("./*.php").on("change", browserSync.reload);
+    gulp.watch("./app/**/*.php").on("change", browserSync.reload);
+}
+
+exports.watch = watch;
